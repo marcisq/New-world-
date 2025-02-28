f4f6cc50032f5025fa9cff84d8833e0b9dabc475
marcisq <marcisupits@gmail.com>
2025-02-28 09:36:37 +0200

    Yes

---
+++ 2Semestris/10.01.2025/Switch.case(28.02.25)/script.js
@@ -0,0 +1,144 @@
+/*
+let m = "augusts";
+
+
+switch(m)
+{
+    case "janvāris":
+    console.log("ziema")
+    break;
+
+
+    case"februāris":
+    console.log("ziema");
+    break;
+
+
+    case "marts":
+    console.log("pavasaris")
+    break;
+
+
+    case "aprīlis":
+    console.log("pavasaris")
+    break;
+
+
+    case "maijs":
+    console.log("pavasaris")
+    break;
+
+
+    case "jūnijs":
+    console.log("vasara")
+    break;
+
+
+    case "jūlijs":
+    console.log("vasara")
+    break;
+
+
+    case "augusts":
+    console.log("vasara")
+    break;
+
+
+    case "septembris":
+    console.log("rudens")
+    break;
+
+
+    case "oktobris":
+    console.log("rudens")
+    break;
+
+
+    case "novembris":
+    console.log("rudens")
+    break;
+
+
+    case "decembris":
+    console.log("ziema")
+    break;
+
+}
+    */
+   /*
+let m = "sestdiena"
+
+switch(m)
+{
+    case"pirmdiena":
+    console.log("darbadiena");
+    break;
+
+    case"otrdiena":
+    console.log("darbadiena");
+
+    case"trešdienaa":
+    console.log("darbadiena");
+    break;
+
+    case"ceturtdiena":
+    console.log("darbadiena");
+    break;
+
+    case"piektdiena":
+    console.log("darbadiena");
+    break;
+
+    case"sestdiena":
+    console.log("brīvdiena");
+    break;
+
+    case"svētdiena":
+    console.log("brīvdiena");
+    break;
+
+}
+    */
+   /*
+   let m = "10"
+
+   switch(m)
+   {
+    case"1":
+    case"2":
+    case"3":
+    case"4":
+    console.log("slikti");
+    break;
+
+    case"5":
+    case"6":
+    case"7":
+    console.log("vidēji");
+    break;
+
+    case"8":
+    case"9":
+    case"10":
+    console.log("labi");
+    break;
+
+   }
+    */
+
+   let m =prompt("ievadi valutu");
+
+   switch(m)
+   {
+    case"USD":
+    console.log("amerikas dolārs");
+    break;
+
+    case"EUR":
+    consolelog("Eiro");
+    break;
+
+    case"GBP":
+    console.log("Lielbritānijas mārciņa");
+    break;
+   }
---
+++ 2Semestris/10.01.2025/script.js
@@ -0,0 +1,30 @@
+/*
+alert("helloworld!");
+
+console.log("Hello World!");
+*/
+/*
+let x = 5;
+let y = 5;
+let result = x % y;
+if(x == y)
+{
+    //console.log("x ir vienāds ar y");
+}
+*/
+
+
+
+let number = prompt("Enter number: ");
+if(number > 0)
+{
+    console.log(number , " ir pozitīvs");
+}
+if (number < 0)
+{
+    console.log(number , " ir negatīvs");
+}
+if (number == 0)
+{
+    console.log(number , "ir 0");
+}
